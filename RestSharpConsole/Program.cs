61b8639e47050ae0f485608de1d6ccfe67f26d2f
Jim Doyle <indellibleink@gmail.com>
2019-07-31 22:27:49 -0400

    working on getting the json data from yahoo

--- RestSharpConsole/Program.cs
+++ RestSharpConsole/Program.cs
@@ -9,23 +9,14 @@ using RestSharp.Authenticators;
 
 namespace RestSharpConsole
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            var restClient = new RestClient("http://ergast.com/api/f1");
+            // Demo Code
+            //WorkingRSharp.RSharpDemo();
 
-            var restRequest = new RestRequest("2016/circuits.json", Method.GET);
-
-            var restResponse = restClient.Execute(restRequest);
-
-            dynamic jsonResponse = JsonConvert.DeserializeObject(restResponse.Content);
-
-            dynamic jsonObject = jsonResponse.MRData.CircuitTable;
-            int circuitId = (int)jsonObject.season;
-            System.Console.WriteLine(jsonObject);
-
-
+            WorkingRSharp.YahooStocks();
 
         }
     }
---
+++ RestSharpConsole/WorkingRSharp.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators;
+using unirest_net.http;
+
+namespace RestSharpConsole
+{
+    public class WorkingRSharp
+    {
+        public static void RSharpDemo()
+        {
+            RestClient restClient = new RestClient("http://ergast.com/api/f1");
+
+            RestRequest restRequest = new RestRequest("2016/circuits.json", Method.GET);
+
+            IRestResponse restResponse = restClient.Execute(restRequest);
+
+            dynamic jsonResponse = JsonConvert.DeserializeObject(restResponse.Content);
+
+            // dynamic jsonObject = jsonResponse.MRData.CircuitTable;
+            //  int circuitId = (int)jsonObject.season;
+            Console.WriteLine(jsonResponse);
+        }
+        public static void YahooStocks()
+        {
+            HttpResponse<MyClass.RootObject> response = Unirest.get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary?region=US&lang=en")
+                                                          .header("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
+                                                          .header("X-RapidAPI-Key", "bd2f89ddc5mshaafba2c2850cce3p1e4c01jsna4733c78a5d4")
+                                                          .asJson<MyClass.RootObject>();
+            dynamic jsonResponse = JsonConvert.DeserializeObject(response.ToString());
+
+            Console.WriteLine(jsonResponse);
+
+            //RestClient restClient = new RestClient("http://ergast.com/api/f1");
+
+            //RestRequest restRequest = new RestRequest("2016/circuits.json", Method.GET);
+
+            //IRestResponse restResponse = restClient.Execute(restRequest);
+
+            //dynamic jsonResponse = JsonConvert.DeserializeObject(restResponse.Content);
+
+            //// dynamic jsonObject = jsonResponse.MRData.CircuitTable;
+            ////  int circuitId = (int)jsonObject.season;
+            //
+        }
+        internal class MyClass
+        {
+            public class Result
+            {
+                public string name { get; set; }
+                public string score { get; set; }
+                public string url { get; set; }
+                public string rlsdate { get; set; }
+                public string rating { get; set; }
+                public string summary { get; set; }
+                public string platform { get; set; }
+            }
+
+            public class RootObject
+            {
+                public List<Result> results { get; set; }
+            }
+        }
+    }
+}
