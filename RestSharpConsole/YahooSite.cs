fc1f2945cef28f52eae15a073d0eb0703d0355fa
Jim Doyle <indellibleink@gmail.com>
2019-08-02 20:59:47 -0400

    working on connection/ getting json reponse

--- RestSharpConsole/YahooSite.cs
+++ RestSharpConsole/YahooSite.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using unirest_net.http;
+using RestSharp.Authenticators;
 
 namespace RestSharpConsole
 {
@@ -15,40 +16,24 @@ namespace RestSharpConsole
     {
         public static void YahooStocks()
         {
-           HttpResponse<Yahoo> response = Unirest.get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary?region=US&lang=en")
-                                                          .header("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
-                                                          .header("X-RapidAPI-Key", "bd2f89ddc5mshaafba2c2850cce3p1e4c01jsna4733c78a5d4");
-                                                         
-            
-            
-             dynamic jsonResponse = JsonConvert.DeserializeObject(response.ToString());
 
-            // Console.WriteLine(response.Headers);
+            //HttpResponse<Yahoo> response = Unirest.get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary?region=US&lang=en")
+            //                                               .header("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
+            //                                               .header("X-RapidAPI-Key", "bd2f89ddc5mshaafba2c2850cce3p1e4c01jsna4733c78a5d4");
 
+            //RestRequest request = new RestRequest("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary?region=US&lang=en", Method.GET);
+            //request.AddHeader("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com");
+            //request.AddHeader("X-RapidAPI-Key", "bd2f89ddc5mshaafba2c2850cce3p1e4c01jsna4733c78a5d4");
+            //request.RequestFormat = DataFormat.Json;
+            string userName = "jfdoyle_iii";
+            string password = "m93Fe8YHn";
+            var restClient = new RestClient("https://finance.yahoo.com/portfolio/p_2/view/v1")
+            {
+                Authenticator = new HttpBasicAuthenticator(userName, password)
+            };
+            // Console.WriteLine(request.RequestFormat.ToString);
+            Console.WriteLine(restClient);
 
-            //          Task<HttpResponse<MyClass>> response = Unirest.post(API_URL)
-            //.header("X-RapidAPI-Key", API_KEY)
-            //.header("Content-Type", "application/x-www-form-urlencoded")
-            //.field("content", File.ReadAllBytes(@"/path/to/file"))
-            //.asJson();
-            //RestClient restClient = new RestClient("http://ergast.com/api/f1");
-
-            //RestRequest restRequest = new RestRequest("2016/circuits.json", Method.GET);
-
-            //IRestResponse restResponse = restClient.Execute(restRequest);
-
-            //dynamic jsonResponse = JsonConvert.DeserializeObject(restResponse.Content);
-
-            //// dynamic jsonObject = jsonResponse.MRData.CircuitTable;
-            ////  int circuitId = (int)jsonObject.season;
-            //
-        }
-
-
-
-        internal class Yahoo
-        {
-            Dictionary<string, string> yahooJson = new Dictionary<string, string>();
         }
     }
 }
