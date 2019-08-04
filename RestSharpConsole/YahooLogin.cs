f7a718df5c2de8a58914012a2e1072e7610ddf22
Jim Doyle <indellibleink@gmail.com>
2019-08-04 11:34:21 -0400

    updated file writer output with time

---
+++ RestSharpConsole/YahooLogin.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using unirest_net.http;
+using RestSharp.Authenticators;
+using System.IO;
+using QuickType;
+
+namespace RestSharpConsole
+{
+    class YahooLogin
+    {
+        public static void Login()
+        {
+            Console.WriteLine("--> Login Authorized Method <--");
+            string userName = "jfdoyle_iii";
+            string password = "m93Fe8YHn";
+            var yahoo = new RestClient("https://finance.yahoo.com/portfolio/p_2/view/v1")
+            {
+                Authenticator = new HttpBasicAuthenticator(userName, password)
+            };
+            RestRequest request = new RestRequest("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary?region=US&lang=en", Method.GET);
+            request.AddHeader("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com");
+            request.AddHeader("X-RapidAPI-Key", "bd2f89ddc5mshaafba2c2850cce3p1e4c01jsna4733c78a5d4");
+
+            IRestResponse restResponse = yahoo.Execute(request);
+
+            dynamic jStocks = JsonConvert.DeserializeObject(restResponse.Content);
+            // var jStocks = JsonConvert.DeserializeObject(restResponse.Content);
+            String jStocksStr = jStocks.ToString();
+
+
+
+            // Console.WriteLine(jStocks);
+
+        }
+    }
+}
--- RestSharpConsole/YahooSite.cs
+++ RestSharpConsole/YahooSite.cs
@@ -12,6 +12,7 @@ using RestSharp.Authenticators;
 using System.IO;
 using QuickType;
 
+
 namespace RestSharpConsole
 {
    public class YahooSite
@@ -32,41 +33,27 @@ namespace RestSharpConsole
 
             dynamic  jStocks= JsonConvert.DeserializeObject(restResponse.Content);
             // var jStocks = JsonConvert.DeserializeObject(restResponse.Content);
-            String jStocksStr = jStocks.ToString();
-
-            var welcome = QuickType.Welcome.FromJson(jStocksStr);
-
-           // Console.WriteLine(jStocks);
-            Console.WriteLine(welcome);
-
-           // ToFile(jStocks);
+            //String jStocksStr = jStocks.ToString();
 
 
-        }
-
-        public static void YahooLogin()
-        {
-            Console.WriteLine("--> Login Authorized Method <--");
-            string userName = "jfdoyle_iii";
-            string password = "m93Fe8YHn";
-            var restClient = new RestClient("https://finance.yahoo.com/portfolio/p_2/view/v1")
-            {
-                Authenticator = new HttpBasicAuthenticator(userName, password)
-            };
-            //RestRequest request = new RestRequest("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary?region=US&lang=en", Method.GET);
-            //request.AddHeader("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com");
-            //request.AddHeader("X-RapidAPI-Key", "bd2f89ddc5mshaafba2c2850cce3p1e4c01jsna4733c78a5d4");
+           
             
-    
+
+      
+            //ToFile(jStocks);
+
 
         }
+
+        
         static void ToFile(dynamic input)
         {
+            String time = DateTime.Now.TimeOfDay.ToString().Replace(":", "!").Remove(8);
+            String folder = @"D:\repository\webScraper\dotNET\";
+            String fileName = "Output_"+time+".txt";
             
-            String Folder = @"D:\repository\webScraper\dotNET\";
-            String FileName = "Output.txt";
 
-            String file = Folder + FileName;
+            String file = folder + fileName;
             StreamWriter streamWriter = new StreamWriter(file);
 
             streamWriter.WriteLine(input);
